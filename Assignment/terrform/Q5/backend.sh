f316b129a9ce7daf7d60b376d22e6a49558b6602
Suryakantapani <suryakantapani2004@gmail.com>
2026-08-23 11:07:11 +0530

    Project start

--- Assignment/terrform/Q5/backend.sh
+++ Assignment/terrform/Q5/backend.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-
 apt update -y
 apt install nginx -y
 systemctl start nginx
 systemctl enable nginx
-
 echo "<h1>Backend Server</h1>" > /var/www/html/index.html
--- Assignment/terrform/Q5/frontend.sh
+++ Assignment/terrform/Q5/frontend.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-
 apt update -y
 apt install nginx -y
 systemctl start nginx
 systemctl enable nginx
-
 echo "<h1>Frontend Server</h1><p>Backend IP: ${backend_ip}</p>" > /var/www/html/index.html
